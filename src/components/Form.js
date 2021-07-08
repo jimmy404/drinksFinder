6e7c950b94781639d44218922ebd50ef4a67c67b
Jimmy404 <bruno_pier@hotmail.com>
2021-07-07 21:56:04 -0300

    Adds basic form and styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,34 @@ import React from 'react';
 
 const Form = () => {
   return (
-    <h1>Form</h1>
-  )
-}
+    <form className="col-12">
+      <fieldset className="text-center">
+        <legend>Search drink by category or ingredient</legend>
+      </fieldset>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            name="name"
+            className="form-control"
+            type="text"
+            placeholder="Search by ingredient"
+          />
+        </div>
+        <div className="col-md-4">
+          <select className="form-control" name="category">
+            <option value="">- Select category -</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Search drinks"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
 
 export default Form;
