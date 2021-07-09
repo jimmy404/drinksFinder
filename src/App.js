110c17170dd67bb248558672605f233bf45da875
Jimmy404 <bruno_pier@hotmail.com>
2021-07-09 16:55:36 -0300

    Adds ProviderModal in App

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import Header from './components/Header';
 import RecipesList from './components/RecipesList';
 
 import ProviderCategories from './context/ContextCategory';
+import ProviderModal from './context/ContextModal';
 import ProviderRecipes from './context/ContextRecipes';
 
 function App() {
   return (
     <ProviderCategories>
       <ProviderRecipes>
-        <Header />
-        <div className="container mt-5">
-          <div className="row">
-            <Form />
+        <ProviderModal>
+
+          <Header />
+
+          <div className="container mt-5">
+            <div className="row">
+              <Form />
+            </div>
+            <RecipesList />
           </div>
-          <RecipesList />
-        </div>
+
+        </ProviderModal>
       </ProviderRecipes>
     </ProviderCategories>
   );
