f0450720b88f2a5aa5834f7ed083afd9dfb47d49
Jimmy404 <bruno_pier@hotmail.com>
2021-07-09 13:21:53 -0300

    Implements categories options in Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -23,6 +23,12 @@ const Form = () => {
         <div className="col-md-4">
           <select className="form-control" name="category">
             <option value="">- Select category -</option>
+            {categories.map(category => (
+              <option
+                key={category.strCategory}
+                value={category.strCategory}
+              >{category.strCategory}</option>
+            ))}
           </select>
         </div>
         <div className="col-md-4">
