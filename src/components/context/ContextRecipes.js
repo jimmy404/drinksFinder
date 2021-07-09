ba19010975b874b861a9684ec44947df06745e68
Jimmy404 <bruno_pier@hotmail.com>
2021-07-09 14:10:11 -0300

    Implements state and call for recipes

--- src/components/context/ContextRecipes.js
+++ src/components/context/ContextRecipes.js
@@ -1,4 +1,6 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 export const ContextRecipes = createContext();
 
@@ -9,12 +11,28 @@ const ProviderRecipes = (props) => {
   const [search, searchRecipes] = useState({
     name: '',
     category: ''
-  })
+  });
+
+  const [consult, saveConsult] = useState(false);
+
+  const { name, category } = search;
+
+  useEffect(() => {
+    if (consult) {
+      const getRecipes = async () => {
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}&c=${category}`;
+        const result = await axios.get(url);
+        setRecipes(result.data.drinks);
+      }
+      getRecipes();
+    }
+  }, [search]);
 
   return (
     <ContextRecipes.Provider
       value={{
-        searchRecipes
+        searchRecipes,
+        saveConsult
       }}
     >
       {props.children}
