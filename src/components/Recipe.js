ac72fe2e446e8c1839f8a70324ef56c22be4a562
Jimmy404 <bruno_pier@hotmail.com>
2021-07-09 17:51:55 -0300

    Implements Modal in Recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,8 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { ContextModal } from '../context/ContextModal';
 
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+// import { ModalConsumer } from '../context/ModalContext';
+
+function getModalStyle() {
+  const top = 50 ;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 const Recipe = ({recipe}) => {
+
+  const [ modalStyle ] = useState(getModalStyle);
+
+  const [ open, setOpen ] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const { saveIdRecipe } = useContext(ContextModal);
 
@@ -21,8 +60,22 @@ const Recipe = ({recipe}) => {
           <button
             type="button"
             className="btn btn-block btn-primary"
-            onClick={() => saveIdRecipe(recipe.idDrink)}
+            onClick={() => {
+              saveIdRecipe(recipe.idDrink);
+              handleOpen();
+            }}
           >Recipe</button>
+          <Modal
+            open={open}
+            onClose={() => {
+              saveIdRecipe(null);
+              handleClose();
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h1>Test - Modal</h1>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
